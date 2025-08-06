c3a0d23776c02123802c6537895d53a43384c777
Dezhyts <dezzhyts@gmail.com>
2025-08-06 17:12:27 +0300

    change all project :)

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:i18next/recommended',
+    'plugin:storybook/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.tsx'] },
+    ],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'comma-dangle': 'off',
+    'react/require-default-props': 'off',
+    'no-shadow': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'object-curly-newline': 'off',
+    'linebreak-style': 'off', // crlf lf
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'i18next/no-literal-string': [
+      'warn',
+      { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
+    ],
+    'max-len': ['error', { code: 120, ignoreComments: true }],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'no-param-reassign': 'off',
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+  overrides: [
+    {
+      files: ['**/*.test.{ts,tsx}'],
+      rules: {
+        'i18next/no-literal-string': 'off',
+      },
+    },
+  ],
+};
---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
@@ -0,0 +1,11 @@
+import { LoginFormProps } from 'features/AuthByUsername/ui/LoginForm/LoginForm';
+import { FC, lazy } from 'react';
+
+export const LoginFormAsync = lazy<FC<LoginFormProps>>(
+  () =>
+    new Promise((resolve) => {
+      // @ts-ignore
+      // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+      setTimeout(() => resolve(import('./LoginForm')), 1500);
+    })
+);
